1d7acce1ff8b5a0ff159620a51dbb7f977a6a0b8
idan sarid <idan.sarid@wdc.com>
2020-10-21 21:42:30 +0300

    herelo be dev
    Signed-off-by: idan sarid <idan.sarid@wdc.com>

--- academic/views.py
+++ academic/views.py
@@ -2,8 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from academic.models import Bulletin, User, Message
+from academic.models import Bulletin
 from datetime import datetime
+from academic.models import Message, User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from academic.serializers import MessageSerializer
+import uuid
 
 
 @login_required(login_url="/login")
@@ -35,13 +41,21 @@ def login_user(request):
 
 
 def register(request):
+    """
+
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
-        if (username == ""): return HttpResponse("No username!")
-        if (password == ""): return HttpResponse("No password!")
-        if (email == ""): return HttpResponse("No email!")
+        if username == "":
+            return HttpResponse("No username!")
+        if password == "":
+            return HttpResponse("No password!")
+        if email == "":
+            return HttpResponse("No email!")
         user = authenticate(username=username, password=password)
         if user is None:
             return HttpResponse("Unautorized!")
@@ -68,15 +82,6 @@ def dashboard(request):
     return render(request, 'admindash.html')
 
 
-from academic.models import Message, Message, User, Sender, Receiver
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from academic.serializers import MessageSerializer, UserSerializer, MessageSerializer, ListSerializer
-import copy
-import uuid
-
-
 @api_view(['GET', 'POST'])
 def message_list(request):
     """
@@ -100,25 +105,24 @@ def write_message(request):
     """
     List all code snippets, or create a new snippet.
     """
-    users = User.objects.all()
-    if request.method == 'GET':
-        messages = Message.objects.all()
-        serializer = MessageSerializer(messages, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = MessageSerializer(data=request.data)
-        message = Message()
-        message.id = uuid.uuid4().int
-        message.creation_date = datetime.now
-        message.sender = str(serializer.initial_data['sender'])
-        message.receiver = str(serializer.initial_data['receiver'])
-        message.message = str(serializer.initial_data['message'])
-        message.subject = str(serializer.initial_data['subject'])
-        message.save()
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        try:
+            serializer = MessageSerializer(data=request.data)
+            message = Message()
+            message.id = uuid.uuid4().int
+            message.creation_date = datetime.now
+            message.sender = str(serializer.initial_data['sender'])
+            message.receiver = str(serializer.initial_data['receiver'])
+            message.message = str(serializer.initial_data['message'])
+            message.subject = str(serializer.initial_data['subject'])
+            message.save()
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            pass
+        finally:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
@@ -179,26 +183,31 @@ def get_all_unread_messages_for_user(request):
         return HttpResponse("No data for user {}".format(request.data))
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def delete_message(request):
     """
-    List all code snippets, or create a new snippet.
+    upon receiving user name and id from request, this function
+    deletes the message if exists
     """
     if request.method == 'POST':
-        expected_user = request.data['user']
-        expected_id = request.data['id']
-        users = User.objects.all()
-        for user in users:
-            if expected_user == user.get_username():
-                messages = Message.objects.filter(receiver=user.get_username())
+        try:
+            expected_user = request.data['user']
+            expected_id = request.data['id']
+            users = User.objects.all()
+            for user in users:
+                if expected_user == user.get_username():
+                    messages = Message.objects.filter(receiver=user.get_username())
+                    message_by_id = messages.get(id=expected_id)
+                    message_by_id.delete()
+                    serializer = MessageSerializer(messages, many=True)
+                    return Response(serializer.data)
+            if expected_user == 'root':
+                messages = Message.objects.all()
                 message_by_id = messages.get(id=expected_id)
                 message_by_id.delete()
-                serializer = MessageSerializer(messages, many=True)
+                serializer = MessageSerializer(message_by_id, many=True)
                 return Response(serializer.data)
-        if expected_user == 'root':
-            messages = Message.objects.all()
-            message_by_id = messages.get(id=expected_id)
-            message_by_id.delete()
-            serializer = MessageSerializer(message_by_id, many=True)
-            return Response(serializer.data)
-        return HttpResponse("Unable to delete a message with id {}".format(expected_id))
+        except Exception as e:
+            pass
+        finally:
+            return HttpResponse("Unable to delete a message with id {}".format(expected_id))
